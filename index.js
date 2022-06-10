b95b18bc42c5a4adf26e3f4170f73c95a46a945b
hcruz <hcruz@compusoluciones.com>
2022-06-10 15:25:40 -0500

    add more option connections

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const openCatalog = require('./lib/OpenCatalog/service');
  * @prototype
  * @class  Icecat
  */
-const icecat = function (username, password, httpUrl = 'data.icecat.biz/xml_s3/xml_server3.cgi') {
+const icecat = function ({ apiToken, username, password, httpUrl = 'data.icecat.biz/xml_s3/xml_server3.cgi' }) {
     this.VERSION = packagejson.version;
     this.scheme = 'https://';
     this.httpAuth = username + ':' + encodeURIComponent(password);
     this.httpUrl = httpUrl;
+    this.apiToken = apiToken;
     this.openCatalog = new openCatalog(this);
 };
 
